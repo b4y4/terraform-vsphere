5fef88db586176662df23e01975cd92b1e823b34
b4y4 <midou_28dz@yahoo.fr>
2020-08-17 17:22:58 +0200

    virtual machine resource terraform file

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-server"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  host_system_id   = data.vsphere_host.host.id
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  #wait_for_guest_net_timeout = 0
+  #wait_for_guest_ip_timeout  = 0
+  #datacenter_id              = data.vsphere_datacenter.dc.id
+
+  #ovf_deploy {
+  #  local_ovf_path       = "${data.vsphere_datacenter.dc.name}/Template/debian9-preconfigured"
+  #  disk_provisioning    = "thin"
+  #  ip_protocol          = "IPV4"
+  #  ip_allocation_policy = "STATIC_MANUAL"
+  #}
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
