ae528e445df21fd82e977cc428b8ba89536926da
b4y4 <midou_28dz@yahoo.fr>
2020-08-18 12:05:49 +0200

    add debian template data

--- data.tf
+++ data.tf
@@ -21,3 +21,9 @@ data "vsphere_host" "host" {
   name          = "172.18.140.50"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_virtual_machine" "template" {
+  name          = "debian9-preconfigured"
+  datacenter_id = data.vsphere_datacenter.dc.id
+
+}
